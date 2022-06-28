58412d1a0d51fbd2462d8e50d8b95178184173c7
Anas Ryadi <anasryadi0@gmail.com>
2022-06-27 18:53:56 -0700

    created routes and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router';
 import './App.css';
+import About from './Components/About';
+import Friends from './Components/Friends';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/friends' element={<Friends />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
