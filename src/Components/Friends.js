db3a1ef8784a87cc5056cf780db1bbe434da3e53
Anas Ryadi <anasryadi0@gmail.com>
2022-06-30 12:49:42 -0700

    added placeholders for activities

--- src/Components/Friends.js
+++ src/Components/Friends.js
@@ -5,8 +5,11 @@ const Friends = () => {
     <div>
       <h1>Plan With Friends</h1>
       <h2>Activity</h2>
+      <input type="activity" placeholder="Suggest an activity" />
       <h2>Restaurant</h2>
+      <input type="restaurant" placeholder="Suggest a restaurant" />
       <h2>Movie</h2>
+      <input type="movie" placeholder="Suggest a movie" />
     </div>
   )
 }
